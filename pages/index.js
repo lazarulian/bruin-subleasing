6c3ce569992adf239bb677a6b4059536eacd73cc
Apurva Shah <shahh.apurva@gmail.com>
2023-02-16 21:31:50 -0800

    Bootstrapped project with create-next-app and tailwind.css

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex justify-center m-10">
+      <h1 className="text-3xl">CS35L Main Project - Initial Commit</h1>
+    </div>
+  )
+}
