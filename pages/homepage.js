0813d7759cd8b34843a3280a4b78f3aaa2cb588d
ChuyQC <jsusortga2016@hotmail.com>
2023-03-11 17:42:49 -0800

    Added InfoCard At bottom of homepage.

--- pages/homepage.js
+++ pages/homepage.js
@@ -1,12 +1,14 @@
 import React, {useState} from 'react'
 import Hero from '@/components/Hero'
 import Navbar from '@/components/Navbar'
+import Infotab from '@/components/Infotab'
 
 function homepage() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Infotab />
     </div>
       
   )
