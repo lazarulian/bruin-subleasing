b7782f0af95c024541682a645818718165708101
ayeshamalvi <aalvi@g.ucla.edu>
2023-03-14 20:10:30 -0700

    fixed Post Path

--- data/posts.js
+++ data/posts.js
@@ -1,12 +1,20 @@
 const posts = [
   {
-    title: "540 Midvale Avenue",
+    title: "540 Kelton Avenue",
     description: `Reimagining compliance and risk screening with solutions powered by network analytics. Exploring better ways to secure the financial system, improve human-machine collaboration and what it means to build a company.`,
     imgSrc: "/static/posts/post1.jpeg",
     quartersAvailable: "Spring",
     poster: "Abishan",
     information:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    amenities: {
+      ac: true,
+      pool: true,
+      parking: false,
+      laundry: false,
+      fitness: true,
+      wifi: true,
+    },
   },
   {
     title: "540 Midvale Avenue",
@@ -16,6 +24,14 @@ const posts = [
     imgSrc: "/static/posts/post2.jpeg",
     information:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    amenities: {
+      ac: false,
+      pool: true,
+      parking: true,
+      laundry: true,
+      fitness: false,
+      wifi: false,
+    },
   },
   {
     title: "522 Landfair Avenue",
@@ -25,6 +41,14 @@ const posts = [
     imgSrc: "/static/posts/post3.jpeg",
     information:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    amenities: {
+      ac: false,
+      pool: false,
+      parking: true,
+      laundry: true,
+      fitness: true,
+      wifi: true,
+    },
   },
   {
     title: "10034 Kelton Avenue",
@@ -34,6 +58,14 @@ const posts = [
     imgSrc: "/static/posts/post4.jpeg",
     information:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    amenities: {
+      ac: false,
+      pool: true,
+      parking: true,
+      laundry: true,
+      fitness: false,
+      wifi: true,
+    },
   },
 ];
 
