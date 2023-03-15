3afe11029505f4ecdea96b87edc6e2bfa57e6698
Apurva Shah <shahh.apurva@gmail.com>
2023-03-15 02:33:31 -0700

    Add sublease overhaul (styling, firebase.)
    - Added utilities to add sublease.
    - Changed styling for add sublease.
    - Added more functionality including snackbar validation.

--- components/SubleaseInput.jsx
+++ components/SubleaseInput.jsx
@@ -1,9 +1,27 @@
-import { React, useState, useEffect } from "react";
+import { React, useState, useEffect, Fragment } from "react";
 import { db } from "../pages/api/firebase-config";
 import { collection, addDoc } from "firebase/firestore";
 import TextField from "@mui/material/TextField";
-import Input from "@mui/material/Input";
+import Checkbox from "@mui/material/Checkbox";
 import { UseAuth } from "@/context/AuthContext";
+
+// MUI Icons
+import AcUnitIcon from "@mui/icons-material/AcUnit";
+import AcUnitIconBorder from "@mui/icons-material/AcUnitOutlined";
+import PoolIcon from "@mui/icons-material/Pool";
+import PoolIconOutlined from "@mui/icons-material/PoolOutlined";
+import DirectionsCarIcon from "@mui/icons-material/DirectionsCar";
+import DirectionsCarIconOutlined from "@mui/icons-material/DirectionsCarOutlined";
+import LocalLaundryServiceIcon from "@mui/icons-material/LocalLaundryService";
+import LocalLaundryServiceIconOutlined from "@mui/icons-material/LocalLaundryServiceOutlined";
+import WifiIcon from "@mui/icons-material/Wifi";
+import WifiIconOutlined from "@mui/icons-material/WifiOutlined";
+import FitnessCenterIcon from "@mui/icons-material/FitnessCenter";
+import FitnessCenterIconOutlined from "@mui/icons-material/FitnessCenterOutlined";
+
+import Snackbar from "@mui/material/Snackbar";
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
 
 const SubleaseInput = () => {
   // Authorization Information
@@ -17,26 +35,63 @@ const SubleaseInput = () => {
   const [numBath, setNumBath] = useState("");
   const [newQuarter, setNewQuarter] = useState("");
   const [imageURL, setImageURL] = useState("");
+  const [roommates, setRoommates] = useState(0);
+  const [squarefootage, setSquareFootage] = useState(0);
 
+  // Amenities
+  const [ac, setAc] = useState(false);
+  const [pool, setPool] = useState(false);
+  const [parking, setParking] = useState(false);
+  const [laundry, setLaundry] = useState(false);
+  const [fitness, setFitness] = useState(false);
+  const [wifi, setWifi] = useState(false);
+
+  // Snackbar
+  const [open, setOpen] = useState(false);
+  const handleClick = () => {
+    setOpen(true);
+  };
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  const action = (
+    <Fragment>
+      <IconButton
+        size="small"
+        aria-label="close"
+        color="inherit"
+        onClick={handleClose}
+      >
+        <CloseIcon fontSize="small" />
+      </IconButton>
+    </Fragment>
+  );
+
+  // AC pool parking laundry fitness wifi
   const clearInput = () => {
     setNewAddress("");
-    setNewRating(NaN);
     setNewRent(0);
     setNewYear(NaN);
-    setNewStreetName("");
     setNumBed("");
     setNumBath("");
     setNewQuarter("");
     setImageURL("");
+    setRoommates(0);
+    setSquareFootage(0);
     document.getElementById("apartment").value = "";
     document.getElementById("rent").value = "";
     document.getElementById("quarter").value = "";
     document.getElementById("year").value = "";
-    document.getElementById("rating").value = "";
-    document.getElementById("street-name").value = "";
     document.getElementById("beds").value = "";
     document.getElementById("baths").value = "";
     document.getElementById("URL").value = "";
+    document.getElementById("roommates").value = "";
+    document.getElementById("sqft").value = "";
   };
 
   const createApartment = async () => {
@@ -49,17 +104,26 @@ const SubleaseInput = () => {
       quarter: newQuarter,
       imageurl: imageURL,
       uid: currentUser.uid,
+      amenities: {
+        ac: ac,
+        pool: pool,
+        parking: parking,
+        laundry: laundry,
+        fitness: fitness,
+        wifi: wifi,
+      },
     });
+    clearInput();
+    handleClick();
   };
 
   return (
-    <div className="grid grid-cols-1 m-8 p-8 .bg-gray-500 gap-3">
-      <h1 class="text-4xl m-3 font-bold text-center"> Add a sublease here: </h1>
+    <div className="grid grid-cols-1 p-8 m-3  rounded-lg gap-3">
       <TextField
         className="rounded-lg border-2 border-blue-400 p-2 m-1"
         placeholder="558 Glenrock Ave"
         type="text"
-        label="Apartment"
+        label="Apartment Address"
         id="apartment"
         required
         onChange={(event) => {
@@ -69,8 +133,8 @@ const SubleaseInput = () => {
       <TextField
         className="rounded-lg border-2 border-blue-400 p-2 m-1"
         type="number"
-        placeholder="3500"
-        label="Rent"
+        placeholder="950"
+        label="Monthly Rent"
         id="rent"
         required
         onChange={(event) => {
@@ -81,7 +145,7 @@ const SubleaseInput = () => {
       <TextField
         className="rounded-lg border-2 border-blue-400 p-2 m-1"
         type="text"
-        placeholder="Fall"
+        placeholder="Spring"
         label="Quarter"
         id="quarter"
         required
@@ -133,7 +197,60 @@ const SubleaseInput = () => {
           setImageURL(event.target.value);
         }}
       />
-
+      <TextField
+        className="rounded-lg border-2 border-blue-400 p-2 m-1"
+        type="number"
+        placeholder="1200"
+        label="Square Footage"
+        id="sqft"
+        required
+        onChange={(event) => {
+          setSquareFootage(event.target.value);
+        }}
+      />
+      <TextField
+        className="rounded-lg border-2 border-blue-400 p-2 m-1"
+        type="number"
+        placeholder="3"
+        label="Number of Roommates"
+        id="roommates"
+        required
+        onChange={(event) => {
+          setRoommates(event.target.value);
+        }}
+      />
+      <div className="flex justify-center">
+        <Checkbox
+          icon={<AcUnitIconBorder />}
+          checkedIcon={<AcUnitIcon />}
+          onChange={() => setAc(!ac)}
+        />
+        <Checkbox
+          icon={<PoolIconOutlined />}
+          checkedIcon={<PoolIcon />}
+          onChange={() => setPool(!pool)}
+        />
+        <Checkbox
+          icon={<DirectionsCarIconOutlined />}
+          checkedIcon={<DirectionsCarIcon />}
+          onChange={() => setParking(!parking)}
+        />
+        <Checkbox
+          icon={<LocalLaundryServiceIconOutlined />}
+          checkedIcon={<LocalLaundryServiceIcon />}
+          onChange={() => setLaundry(!laundry)}
+        />
+        <Checkbox
+          icon={<WifiIconOutlined />}
+          checkedIcon={<WifiIcon />}
+          onChange={() => setWifi(!wifi)}
+        />
+        <Checkbox
+          icon={<FitnessCenterIcon />}
+          checkedIcon={<FitnessCenterIconOutlined />}
+          onChange={() => setFitness(!fitness)}
+        />
+      </div>
       <button
         className="bg-blue-100 rounded-md p-3 m-2 hover:bg-blue-600 hover:text-white duration-200"
         onClick={createApartment}
@@ -147,6 +264,13 @@ const SubleaseInput = () => {
       >
         Clear Input
       </button>
+      <Snackbar
+        open={open}
+        autoHideDuration={6000}
+        onClose={handleClose}
+        message="✅  Sublease Submitted  ✅ "
+        action={action}
+      />
     </div>
   );
 };
--- pages/add-sublease.js
+++ pages/add-sublease.js
@@ -1,9 +1,30 @@
+import React from "react";
 import SubleaseInput from "@/components/SubleaseInput";
+import Image from "next/image";
 
 export default function AddSublease() {
   return (
-    <div className="subleaseInput">
-      <SubleaseInput />
+    <div className="px-12 py-8 h-screen bg-slate-100 align-middle content-center">
+      <div className="container mx-auto lg:px-10 mb-8 px-2 ">
+        <div className="grid lg:grid-cols-2 lg:gap-3 bg-white rounded-xl">
+          <div className="text-center content-center align-middle p-8 ">
+            <h1 className="font-semibold text-gray-700 text-4xl">
+              Add Sublease
+            </h1>
+            <SubleaseInput />
+          </div>
+          <div className="col-span-1">
+            <div className="flex h-full align-bottom items-center justify-center ">
+              <Image
+                src="/static/images/uploadImage.png"
+                alt="Upload Image"
+                width={800}
+                height={800}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
