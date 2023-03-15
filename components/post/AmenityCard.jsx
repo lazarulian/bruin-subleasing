13c58211289f221aa80f97238c2e9dd8318d77f7
ayeshamalvi <aalvi@g.ucla.edu>
2023-03-14 22:59:41 -0700

    changed amenities card

--- components/post/AmenityCard.jsx
+++ components/post/AmenityCard.jsx
@@ -8,44 +8,95 @@ import FitnessCenterIcon from "@mui/icons-material/FitnessCenter";
 
 const AmenityCard = ({ ac, pool, parking, laundry, fitness, wifi }) => {
   return (
-    <div className="rounded-lg border-2 drop-shadow-md mx-auto mr-0 h-72 w-96 bg-slate-50 hover:bg-slate-100 duration-300 ease-in-out text-center">
-      <p className="pt-5 pb-7 text-xl"> Amenities </p>
-      {ac && (
-        <div className="flex flex-row ml-28">
-          <AcUnitIcon className="h-6 w-6"> </AcUnitIcon>
-          <p className="pb-5 indent-2 text-l">Air Conditioning</p>
-        </div>
-      )}
-      {pool && (
-        <div className="flex flex-row ml-28">
-          <PoolIcon className="h-6 w-6"> </PoolIcon>
-          <p className="pb-5 indent-2 text-l"> Pool</p>
-        </div>
-      )}
-      {parking && (
-        <div className="flex flex-row ml-28">
-          <DirectionsCarIcon className="h-6 w-6"> </DirectionsCarIcon>
-          <p className="pb-5 indent-2 text-l"> Parking</p>
-        </div>
-      )}
-      {laundry && (
-        <div className="flex flex-row ml-28">
-          <LocalLaundryServiceIcon className="h-6 w-6"></LocalLaundryServiceIcon>
-          <p className="pb-5 indent-2 text-l"> Washer/Dryer</p>
-        </div>
-      )}
-      {fitness && (
-        <div className="flex flex-row ml-28">
-          <FitnessCenterIcon className="h-6 w-6"></FitnessCenterIcon>
-          <p className="pb-5 indent-2 text-l"> Fitness</p>
-        </div>
-      )}
-      {wifi && (
-        <div className="flex flex-row ml-28">
-          <WifiIcon className="h-6 w-6"></WifiIcon>
-          <p className="pb-5 indent-2 text-l"> Wifi</p>
-        </div>
-      )}
+    <div>
+      <div class="w-full mx-auto max-w-sm p-4 bg-white border border-gray-200 rounded-lg sm:p-6 dark:bg-gray-800 dark:border-gray-700 drop-shadow-xl">
+        <h5 class="mb-3 text-base font-semibold text-gray-900 md:text-xl dark:text-white">
+          Amenities
+        </h5>
+
+        <ul class="my-4 space-y-3">
+          {ac && (
+            <li>
+              <a
+                href="#"
+                class="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white"
+              >
+                <span class="flex-1 ml-3 whitespace-nowrap">
+                  Air Conditioning
+                </span>
+                <span class="inline-flex items-center justify-center px-2 py-0.5 ml-3 text-xs font-medium text-gray-500 bg-gray-200 rounded dark:bg-gray-700 dark:text-gray-400">
+                  <AcUnitIcon />
+                </span>
+              </a>
+            </li>
+          )}
+          {pool && (
+            <li>
+              <a
+                href="#"
+                class="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white"
+              >
+                <span class="flex-1 ml-3 whitespace-nowrap">Pool</span>
+                <span class="inline-flex items-center justify-center px-2 py-0.5 ml-3 text-xs font-medium text-gray-500 bg-gray-200 rounded dark:bg-gray-700 dark:text-gray-400">
+                  <PoolIcon />
+                </span>
+              </a>
+            </li>
+          )}
+          {parking && (
+            <li>
+              <a
+                href="#"
+                class="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white"
+              >
+                <span class="flex-1 ml-3 whitespace-nowrap">Parking</span>
+                <span class="inline-flex items-center justify-center px-2 py-0.5 ml-3 text-xs font-medium text-gray-500 bg-gray-200 rounded dark:bg-gray-700 dark:text-gray-400">
+                  <DirectionsCarIcon />
+                </span>
+              </a>
+            </li>
+          )}
+          {laundry && (
+            <li>
+              <a
+                href="#"
+                class="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white"
+              >
+                <span class="flex-1 ml-3 whitespace-nowrap">Laundry</span>
+                <span class="inline-flex items-center justify-center px-2 py-0.5 ml-3 text-xs font-medium text-gray-500 bg-gray-200 rounded dark:bg-gray-700 dark:text-gray-400">
+                  <LocalLaundryServiceIcon />
+                </span>
+              </a>
+            </li>
+          )}
+          {wifi && (
+            <li>
+              <a
+                href="#"
+                class="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white"
+              >
+                <span class="flex-1 ml-3 whitespace-nowrap">Wifi</span>
+                <span class="inline-flex items-center justify-center px-2 py-0.5 ml-3 text-xs font-medium text-gray-500 bg-gray-200 rounded dark:bg-gray-700 dark:text-gray-400">
+                  <WifiIcon />
+                </span>
+              </a>
+            </li>
+          )}
+          {fitness && (
+            <li>
+              <a
+                href="#"
+                class="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white"
+              >
+                <span class="flex-1 ml-3 whitespace-nowrap">Fitness</span>
+                <span class="inline-flex items-center justify-center px-2 py-0.5 ml-3 text-xs font-medium text-gray-500 bg-gray-200 rounded dark:bg-gray-700 dark:text-gray-400">
+                  <FitnessCenterIcon />
+                </span>
+              </a>
+            </li>
+          )}
+        </ul>
+      </div>
     </div>
   );
 };
